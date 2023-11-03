f5fb6d2ac36b31c837877d35014b2049e012b6ca
Elton <68150154+aliyevelton@users.noreply.github.com>
2023-11-03 20:49:38 +0400

    Add files via upload

---
+++ arraypattern/arraypatter/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();
+
+string  Pattern(ref string input)
+{
+    string result = string.Empty;
+    string result0 = string.Empty;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        char c = input[i];
+        result0 += c;
+        result += result0;
+    }
+    return result;
+}
+
+Console.WriteLine(Pattern(ref input));
